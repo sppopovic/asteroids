733d0ca49fc20c0fa5e797d62633ca1ad1b0ffbe
sppopovic <s.p.popovic@gmail.com>
2024-08-24 14:23:13 +0200

    Added pygame window

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print(f"Starting asteroids!")
+    print(f"Screen width:", SCREEN_WIDTH)
+    print(f"Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
